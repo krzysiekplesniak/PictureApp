f58da7e607d37a2e90c5bd8b4dd357c448147ef4
KrzysiekP <krzysiek@bejamas.io>
2021-11-25 16:52:44 +0100

    Added Strapi backend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c525a205b2d75d51098ee7582c238500'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '183e541d-de19-41ff-a5cc-4bc2c2c8dc90'
+};
