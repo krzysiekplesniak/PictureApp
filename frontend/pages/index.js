0b4b18d26607aefff4239a5d2eb629d3ae473e56
KrzysiekP <krzysiek@bejamas.io>
2021-11-25 15:31:09 +0100

    Added dummies data for a while in /api/pictures

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,14 +1,28 @@
 import Layout from "@/components/Layout"
-import { API_URL } from "@/config"
+import { API_URL } from "@/config/index"
 import Link from "next/link"
 
 
-export default HomePage = () => {
+export default function HomePage({ pictures }) {
+  
+  console.log('FRONT', pictures);
+
   return (
    
     <Layout title="Home page | PictureApp">
 
-      <Link href="/about">About page</Link>
+      <h1>All Pictures</h1>
+
+      {pictures.length === 0 && <h3>No pictures at all</h3>}
+
+            
+      {pictures.map(p => (
+        <>
+          <p>{p.id}</p>
+          <p>{p.name}</p>
+        </>
+      ))}
+      
 
     </Layout>
   )
@@ -16,12 +30,12 @@ export default HomePage = () => {
 
 export async function getServerSideProps() {
 
-  const reposne = await fetch(`${API_URL}/api/pictures`); 
+  const response = await fetch(`${API_URL}/api/pictures`); 
   const pictures = await response.json();
 
-  console.log(pictures);
+  //console.log('BACK',pictures);
   
   return {
-    props: {}
+    props: { pictures},
   }
 }
